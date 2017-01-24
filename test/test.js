6936b371cd1de65fe7c28c5949e9a6c1c8d48eae
you21979 <you2197901@gmail.com>
2017-01-24 18:46:00 +0900

    add test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,42 @@
 'use strict';
 var assert = require('assert');
-var fees = require('..');
+var les = require("local-echoserver");
+var bitcoinfees = require('..');
 
 describe('test', function () {
     
     describe('test', function () {
+
+        it('recommended', function () {
+            return les(function(url){
+                bitcoinfees.Constant.API_BASE_URL = url + "/api/v1";
+                return bitcoinfees.FeesApi.recommended().then(function(res){
+                    assert(res.fastestFee === 40);
+                })
+            }, function(res, headers, method, url, body){
+                assert(method === 'GET');
+                assert(url === '/api/v1/fees/recommended');
+                var result = JSON.stringify({
+                    fastestFee: 40,
+                    halfHourFee: 20,
+                    hourFee: 10
+                });
+                res.end(result);
+            })
+        })
+        it('list', function () {
+            return les(function(url){
+                bitcoinfees.Constant.API_BASE_URL = url + "/api/v1";
+                return bitcoinfees.FeesApi.list().then(function(res){
+                })
+            }, function(res, headers, method, url, body){
+                assert(method === 'GET');
+                assert(url === '/api/v1/fees/list');
+                var result = JSON.stringify({
+                });
+                res.end(result);
+            })
+        })
+
     })
 })
